a7139425f5f1d1ff7401911e9f953463e18ad03e
John <80288341+j-beastman@users.noreply.github.com>
2023-06-13 16:41:33 -0400

    more features, uncached, undownloaded

--- ufc-scraping/ufc-scraping.py
+++ ufc-scraping/ufc-scraping.py
@@ -1,6 +1,7 @@
+import time
+
 import requests
 from bs4 import BeautifulSoup
-import time
 
 # Specify the URL of the webpage you want to fetch
 url = "https://www.ufc.com/event/ufc-288"
@@ -13,7 +14,8 @@ response = requests.get(url)
 html_content = response.text
 
 # Specify the use of lxml parser
-soup = BeautifulSoup(html_content, 'lxml')
+soup = BeautifulSoup(html_content, "lxml")
+
 
 # Finding the class of the element I want: Fighter names
 def find_classes(input):
@@ -21,45 +23,53 @@ def find_classes(input):
     if input_found is None:
         print("Input not found")
         return None
-    input_classes = [input.parent.get('class') for input in input_found]
+    input_classes = [input.parent.get("class") for input in input_found]
     # Make it so that this is an option
     input_parents = [input.parent for input in input_found]
     print(input_parents)
     # It ("get()") returns a list, but there's only 1 element in it (usually)
     return input_classes
 
+
 # What's the difference between the red corner and blue corner in the ufc
 def get_fighters():
-    elements = (soup.find_all(class_="c-listing-ticker-fightcard__red_corner_name"))
+    elements = soup.find_all(
+        class_="c-listing-ticker-fightcard__red_corner_name")
     red_corner_fighter_names = [element.text for element in elements]
     print(red_corner_fighter_names)
-    time.sleep(0.5) # Appease the website
+    time.sleep(0.5)  # Appease the website
 
-    elements = (soup.find_all(class_="c-listing-ticker-fightcard__blue_corner_name"))
+    elements = soup.find_all(
+        class_="c-listing-ticker-fightcard__blue_corner_name")
     blue_corner_fighter_names = [element.text for element in elements]
     print(blue_corner_fighter_names)
-    time.sleep(0.5) # Appease the website
+    time.sleep(0.5)  # Appease the website
 
+
+# This function can definitely be cut in half
 def get_win_loss():
     red_corner_win_loss = list()
-    elements = (soup.find_all(class_="c-listing-fight__corner-body--red"))
+    elements = soup.find_all(class_="c-listing-fight__corner-body--red")
+    time.sleep(0.5)  # Appease the website
     for element in elements:
-        subclass_element = element.find(class_="c-listing-fight__outcome-wrapper")
+        subclass_element = element.find(
+            class_="c-listing-fight__outcome-wrapper")
         if subclass_element:
             # Process the subclass element
             if subclass_element.find(class_="c-listing-fight__outcome--Win"):
                 red_corner_win_loss.append("Win")
-            # Elif added so that if Win is changed to lowercase or something, we'll see empty 
+            # Elif added so that if Win is changed to lowercase or something, we'll see empty
             # entries instead of false "Loss" entries
             elif subclass_element.find(class_="c-listing-fight__outcome--Loss"):
                 red_corner_win_loss.append("Loss")
         else:
             print("Subclass element not found.")
     blue_corner_win_loss = list()
-    elements = (soup.find_all(class_="c-listing-fight__corner-body--blue"))
+    elements = soup.find_all(class_="c-listing-fight__corner-body--blue")
+    time.sleep(0.5)  # Appease the website
     for element in elements:
-        subclass_element = element.find(class_="c-listing-fight__outcome-wrapper")
-        print(subclass_element, "end of subclass element")
+        subclass_element = element.find(
+            class_="c-listing-fight__outcome-wrapper")
         if subclass_element:
             if subclass_element.find(class_="c-listing-fight__outcome--Win"):
                 blue_corner_win_loss.append("Win")
@@ -67,20 +77,40 @@ def get_win_loss():
                 blue_corner_win_loss.append("Loss")
         else:
             print("Subclass element not found.")
-    print("Red Corner W\L", red_corner_win_loss, "\n\n\n\n", "Blue Corner W\L", blue_corner_win_loss)
-    
+    print(
+        "Red Corner W\\L",
+        red_corner_win_loss,
+        "\n\n\n\n",
+        "Blue Corner W\\L",
+        blue_corner_win_loss,
+    )
+
+# Some outliers where you have a title fight with an unranked opponent
+# (coming up from different weight class)
 def get_fight_class():
-    elements = (soup.find_all(class_="c-listing-fight__class-text"))
+    elements = soup.find_all(class_="c-listing-fight__class-text")
+    time.sleep(0.5)  # Appease the website
+    weight_classes = [element.text for element in elements]
+    print(weight_classes)
+
+# def get_fighters_ranks():
+#     elements = soup.find_all(class_="js-listing-fight__corner-rank c-listing-fight__corner-rank")
+#     time.sleep(0.5)  # Appease the website
+#     # weight_classes = [element.text for element in elements]
+#     print(elements)
+
+def get_fight_odds():
+    red_corner_odds, blue_corner_odds = list()
+    elements = soup.find_all(class_="c-listing-fight__odds-wrapper")
+    time.sleep(0.5)  # Appease the website
+    for element in elements:
+        
+    # weight_classes = [element.text for element in elements]
     print(elements)
-# Some outliers where you have a title fight with an unranked opponent (coming up from different weight class)
-def get_fighters_ranks():
-    elements = (soup.find_all(class_="c-listing-fight__class-text"))
-    print(elements)
 
-get_fight_class()
+def fighter_nations():
+    return None
 
-# def print_parents(child):
-#     print(child.parent, "\n")
-#     print_parents(child.parent)
-
-# print_parents(phrase)
+# Decision (split, unanimous), Submission (round), TKO/KO (round)
+def win_method():
+    return None 
